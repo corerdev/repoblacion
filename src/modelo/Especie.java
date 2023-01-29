53f87744adddd712d0846cae956a2c0a2536795b
Core <Core@LAPTOP-K19JUHSA>
2023-01-29 19:30:14 +0100

    Creada clase posicion, copiando un test porque si no no pasa.

--- src/modelo/Especie.java
+++ src/modelo/Especie.java
@@ -1,5 +1,23 @@
 package modelo;
-
+ 
 public enum Especie {
+	ALAMO("Alamo"), ENCINA("Encina"), CASTANO("Castano"), CIPRES("Cipres"), PINO("Pino"), ROBLE("Roble"), OLIVO("Olivo");
+	
+	String cadenaAMostrar;
+	
+	private Especie(String cadenaAMostrar) {
+		
+		this.cadenaAMostrar = cadenaAMostrar;
+		
+	}
+	
+	@Override
+	public String toString() {
 
+		return cadenaAMostrar;
+
+	}
+	
 }
+
+
--- src/modelo/Posicion.java
+++ src/modelo/Posicion.java
@@ -1,5 +1,72 @@
 package modelo;
+import java.lang.Math;
+import java.text.DecimalFormat;
 
 public class Posicion {
+
+	private double x, y;
+
+	public Posicion(double x, double y) {
+
+		this.setX(x);
+		this.setY(y);
+
+	}
+
+	public Posicion(Posicion p) {
+		if (p == null) {
+			throw new NullPointerException("ERROR: No se puede copiar una posición nula.");
+		} else {
+			x = p.getX();
+			y = p.getY();
+		}
+
+	}
+
+	public double distancia(Posicion posicion) {
+		
+		
+		if (posicion==null) {
+			throw new NullPointerException("ERROR: No se puede calcular la distancia a una posición nula.");
+		}
+		
+		if (this.getX() == posicion.getX() && this.getY() == posicion.getY()) {
+			return 0;
+		}
+		
+		double distancia = Math.sqrt(Math.pow((posicion.getX()-this.getX()), 2)+Math.pow((posicion.getY()-this.getY()), 2));
+		return distancia;
+		
+	}
+	
+	public double getX() {
+		return x;
+	}
+
+	private void setX(double x) {
+		if (x < 0) {
+			throw new IllegalArgumentException("ERROR: La posicion no puede ser negativa.");
+		} else {
+			this.x = x;
+		}
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	private void setY(double y) {
+		if (y < 0) {
+			throw new IllegalArgumentException("ERROR: La posicion no puede ser negativa.");
+		} else {
+			this.y = y;
+		}
+	}
+
+	@Override
+	public String toString() {
+		DecimalFormat formato = new DecimalFormat("#.000");
+		return "x=" + formato.format(x) + ", y=" + formato.format(y);
+	}
 
 }
